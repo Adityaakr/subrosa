95c0d7e9ce33ff3bbc6fedb4d127649d1a577b62
Adityaakr <adityakrx7@gmail.com>
2026-06-15 22:50:00 +0530

    feat(web): wire app to Subrosa view

---
+++ web/app/src/App.tsx
@@ -0,0 +1,10 @@
+import { AppProviders } from "@/providers";
+import { Subrosa } from "@/components/Subrosa";
+
+export default function App() {
+  return (
+    <AppProviders>
+      <Subrosa />
+    </AppProviders>
+  );
+}
